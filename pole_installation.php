acc9306815e638ff6bdb6ec2744df8551aa986d7
victor-home-pc <volsavaivictor@gmail.com>
2025-10-14 15:23:58 +1000

    Add: Map Layer Using Leaflet Lib for OpenStreetMap

--- pole_installation.php
+++ pole_installation.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./inc/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <title>Add Pole Installation</title>
 </head>
 <body>
@@ -14,5 +15,31 @@
         <input type="text" name="zone" class="form-control mb-2" placeholder="Enter Zone">
         <input type="submit" name="submit" class="btn btn-primary">
     </form>
+
+    <div id="mapCont">
+    </div>
+
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 </body>
 </html>
+
+<style>
+    #mapCont {
+        width: 90%;
+        margin: 0 auto;
+        margin-bottom: 3rem;
+        aspect-ratio: 2/1;
+    }
+</style>
+
+
+<script>
+    let mapOptions = {
+        center: [-9.45142, 147.19585],
+        zoom: 13
+    }
+
+    let map = new L.map('mapCont', mapOptions)
+    let layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    map.addLayer(layer)
+</script>
