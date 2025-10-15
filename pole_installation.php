9bfd8427090ba604b4882227a9d5cd791a5ea1d9
victor-home-pc <volsavaivictor@gmail.com>
2025-10-16 08:45:52 +1000

    Add Marker Popup Feature

--- pole_installation.php
+++ pole_installation.php
@@ -83,6 +83,13 @@
         height: calc(100vh - 65px);
 
         position: relative;
+
+        #polePopup {
+            & p {
+                margin: 0;
+                padding: 0;
+            }
+        }
     }
 
     #mapCont {
@@ -104,8 +111,9 @@
 
     let customIcon = {
         iconUrl: "./inc/img/ofc-pole3.png",
-        iconSize: [30, 30],
-        iconAnchor: [15,30]
+        iconSize: [40, 40],
+        iconAnchor: [20,40],
+        popupAnchor: [0,-40]
     }
 
     let myIcon = L.icon(customIcon)
@@ -115,13 +123,27 @@
     }
 
     //? Pull In Pole Coordinates & Add Marker to Map
-
     let poles = $('#polesData').attr('data-poles')
     poles = JSON.parse(poles)
 
     poles.forEach((i, idx)=>{
-        temp = new L.Marker([parseFloat(i[1]), parseFloat(i[2])], iconOptions)
-        temp.addTo(map)
+        //? Add Marker to Map
+        new L.Marker([parseFloat(i[1]), parseFloat(i[2])], iconOptions).addTo(map)
+        
+        //? Add PopUp Content
+        .on("mouseover", event => {
+            event.target.bindPopup(`
+                <div id="polePopup">
+                    <p>ID: ${i[0]}</p>
+                    <p>Lat: ${parseFloat(i[1])}</p>
+                    <p>Lng: ${parseFloat(i[2])}</p>
+                    <p>Zone: ${i[3]}</p>
+                </div>`, {'closeButton': false})
+            .openPopup()
+        })
+        .on("mouseout", event => {
+            event.target.closePopup()
+        })
     })
 
     //? Event to trigger when a location on map is clicked
