3d8696e39b53baefffbdf14090d5e36c6d44efda
victor-home-pc <volsavaivictor@gmail.com>
2025-10-21 14:31:44 +1000

    New: Side Menu Temp

--- cmsofc.php
+++ cmsofc.php
@@ -33,17 +33,7 @@
         <div id="mapCont">
         </div>
 
-        <div class="sideMenuBar p-3">
-            <button class="btn btn-sm btn-warning w-75 mb-2" onclick="addPole()">Add Pole</button>
-            <button class="btn btn-sm btn-warning w-75 mb-2" onclick="addJB()">Add Enclosure</button>
-            <button class="btn btn-sm btn-warning w-75 mb-2" onclick="addPole()">Add Splitter</button>
-            <button class="btn btn-sm btn-warning w-75 mb-2" onclick="addPole()">Add Cable</button>
-            <button class="btn btn-sm btn-warning w-75 mb-2" onclick="addPole()">Add OLT</button>
-            <button class="btn btn-sm btn-warning w-75 mb-2" onclick="addPole()">Add ONU</button>
-            <br><br>
-            <button class="btn btn-sm btn-warning w-75 mb-2" onclick="pomVw()">POM</button>
-            <button class="btn btn-sm btn-warning w-75 mb-2" onclick="mdgVw()">MADANG</button>
-        </div>
+        <?php include './inc/temp/temp_sideMenuBar.php'; ?>
 
         <?php include './inc/temp/temp_addPole.php'; ?>
         <?php include './inc/temp/temp_addJuncBox.php'; ?>
@@ -72,17 +62,6 @@
                     padding: 0;
                 }
             }
-        }
-
-        .sideMenuBar {
-            width: 200px;
-            height: 100%;
-
-            background-color: skyblue;
-
-            display: flex;
-            flex-direction: column;
-            align-items: center;
         }
 
         #mapCont {
