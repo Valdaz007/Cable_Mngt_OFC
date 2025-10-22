c4f39d4b6199eaf4793082e9419ca980c24e5f4a
victor-home-pc <volsavaivictor@gmail.com>
2025-10-22 14:21:00 +1000

    Update: Model Temp UI

--- inc/temp/temp_addJuncBox.php
+++ inc/temp/temp_addJuncBox.php
@@ -1,16 +1,23 @@
 
 <form class="add_JuncBox_Form" action="./inc/func/func_updateJBTbl.php" method="POST" class="p-5">
-    <h2>Junction Box Installation</h2>
-    <select name="poleID" id="" class="form-control mb-2">
-        <?php
-            $poles = getPoles();
+    <h6>Junction Box Installation</h6>
+    <div class="form-floating mb-2">
+        <select name="poleID" id="pole-id" class="form-control">
+            <?php
+                $poles = getPoles();
+        
+                while($row = mysqli_fetch_assoc($poles)) { 
+            ?>
+                <option value="<?php echo $row['pole_id'];?>"><?php echo $row['pole_zone'].'-'.$row['pole_id'];?></option>
+            <?php } ?>
+        </select>
+        <label for="pole-id">Pole ID</label>
+    </div>
+    <div class="form-floating mb-2">
+        <input type="text" name="jb_desc" class="form-control" id="jb-desc" placeholder="Enter Description">
+        <label for="jb-desc">Pole Description</label>
+    </div>
 
-            while($row = mysqli_fetch_assoc($poles)) { 
-        ?>
-            <option value="<?php echo $row['pole_id'];?>"><?php echo $row['pole_zone'].'-'.$row['pole_id'];?></option>
-        <?php } ?>
-    </select>
-    <input type="text" name="jb_desc" class="form-control mb-2" id="pole-lat" placeholder="Enter Description">
     <input type="submit" name="submit" class="btn btn-primary">
 </form>
 
@@ -19,7 +26,7 @@
         display: none;
 
         width: 450px;
-        padding: 2rem;
+        padding: 1rem;
 
         background-color: orange;
         border-radius: 1rem;
--- inc/temp/temp_addPole.php
+++ inc/temp/temp_addPole.php
@@ -1,8 +1,17 @@
 <form class="add_Pole_Form" action="./inc/func/func_updatePoleTbl.php" method="POST" class="p-5">
-    <h2>Pole Installation</h2>
-    <input type="text" name="coordx" class="form-control mb-2" id="pole-lat" placeholder="Enter Pole Latitude">
-    <input type="text" name="coordy" class="form-control mb-2" id="pole-lng" placeholder="Enter Pole Longitude">
-    <input type="text" name="zone" class="form-control mb-2" placeholder="Enter Zone">
+    <h6>Pole Installation</h6>
+    <div class="form-floating">
+        <input type="text" name="coordx" class="form-control mb-2" id="pole-lat" placeholder="Enter Pole Latitude">
+        <label for="pole-lat">Pole Latitude</label>
+    </div>
+    <div class="form-floating">
+        <input type="text" name="coordy" class="form-control mb-2" id="pole-lng" placeholder="Enter Pole Longitude">
+        <label for="pole-lng">Pole Longitude</label>
+    </div>
+    <div class="form-floating">
+        <input type="text" name="zone" class="form-control mb-2" id="pole-zone" placeholder="Enter Zone">
+        <label for="pole-zone">Pole Zone</label>
+    </div>
     <input type="submit" name="submit" class="btn btn-primary">
 </form>
 
@@ -11,7 +20,7 @@
         display: none;
 
         width: 450px;
-        padding: 2rem;
+        padding: 1rem;
 
         background-color: orange;
         border-radius: 1rem;
